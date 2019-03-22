6804840987ccf87b58ed7eeabc43fb3e107ca976
Telmo Rodrigues <telmo.galante.rodrigues@gmail.com>
2019-03-22 01:59:06 +0000

    first qrcode

--- app/lastcard/admin.py
+++ app/lastcard/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Card
 
 class CardAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        return super(CardAdmin, self).save_model(request, obj, form, change)
 
 admin.site.register(Card, CardAdmin)
