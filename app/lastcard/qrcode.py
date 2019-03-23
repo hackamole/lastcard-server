413ae53b06ec2ce02ccd09b9a82b3b9c4b214300
Sofia Cardita <sofiacardita@gmail.com>
2019-03-23 12:35:06 +0000

    All new cards point to same place

--- app/lastcard/qrcode.py
+++ app/lastcard/qrcode.py
@@ -5,7 +5,8 @@ import qrcode.image.svg
 
 
 def generate(base_url, base_path, card_id):
-    url = urljoin(base_url, '/'.join(['card', card_id]))
+    # url = urljoin(base_url, '/'.join(['card', card_id]))
+    url = urljoin(base_url, '/'.join(['card'])) # MAJOR HACK
     img = qrcode.make(url, image_factory=qrcode.image.svg.SvgImage)
-    
+
     img.save(os.path.join(base_path, card_id + ".svg"))
